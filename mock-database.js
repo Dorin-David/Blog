019d746ba1e741c27771d3df03233aab5878ec2f
Dorin David <70648503+Dorin-David@users.noreply.github.com>
2021-05-25 08:47:56 +0200

    add pagination

--- mock-database.js
+++ mock-database.js
@@ -89,6 +89,93 @@ export const database = [
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a velit gravida, tristique leo quis, lacinia quam. Praesent a metus quis turpis blandit tristique. Cras vitae urna vulputate, porttitor elit et, luctus mi.',
         position: 'center bottom'
     },
+    {
+        title: 'The Naked Lunch',
+        author: 'Bruno Lusardi',
+        author_path: 'autori/bruno_lusardi',
+        article_path: '/articoli/naked-lunch',
+        img: '/assets/burroughs_cronnenberg.jpeg',
+        description: `David Cronenberg, canadese interessato alla scienza ed alla
+        fantascienza, agli sbocchi della carne ed alla sua contaminazione, vide in William Burroughs un magnete –
+        ovvero un potente centro di fascinazione.`,
+        position: 'center bottom'
+    },
+    {
+        title: 'Pulp Fiction',
+        author: 'Bruno Lusardi',
+        author_path: 'autori/bruno_lusardi',
+        article_path: '/articoli/pulp-fiction',
+        img: '/assets/pulp_fiction.jpeg',
+        description: `Siamo negli anni ’90, e in pochi si possono vantare di poter scrivere e dirigere una simile, geniale e deliziosa cazzata.`,
+        
+    },
+    {
+        title: `L'artista`,
+        header: 'Ovvero il lebbroso',
+        author: 'Dorin David',
+        author_path: 'autori/dorin_david',
+        article_path: '/articoli/artista',
+        img: '/assets/prometheus-1883_bocklin.jpg',
+        description: `"E’ proprio nella misura in cui sono inconsci, semplici strumenti, che il loro lavoro è fondamentale; 
+        essi sono un ponte tra l’ordinario e il Mistero."`,
+        position: 'center top'
+
+    },
+    {
+        title: 'Le voci',
+        header: 'Racconto',
+        author: 'Dorin David',
+        author_path: 'autori/dorin_david',
+        article_path: '/racconti/le-voci-dorin-david',
+        img: '/assets/night-1890.jpg',
+        description: `Il peggio era quando il loro vociare si mischiava a quello dei vivi;
+        e dato che il tono e le sembianze (quando ne avevano) erano, pure, simili, 
+        non riusciva piu' a distinguere gli uni dagli altri.`,
+        position: 'center -40px'
+
+    },
+    {
+        title: 'La Ruota',
+        header: 'Ma in fondo, vogliamo star meglio?',
+        author: 'Dorin David',
+        author_path: 'autori/dorin_david',
+        article_path: '/articoli/la-ruota',
+        img: '/assets/ferris-wheel-in-the-tuileries-1905.jpg',
+        description: `Le leggi dell’universo non cambieranno mai, per i pezzenti e per gli dèi: prima in alto, poi in basso, la Ruota gira e noi siamo da capo. 
+        Comprenderlo? Abbiamo la memoria corta… `,
+    },
+    {
+        title: 'Segni',
+        header: 'Pensieri in una notte di primavera',
+        author: 'Dorin David',
+        author_path: 'autori/dorin_david',
+        article_path: '/articoli/segni',
+        img: '/assets/memory-1948_magritte.jpg',
+        description: `E mi sembra tutto bello, tutto necessario, nella mia stupenda casa in una notte primaverile, chissà che cosa stanno faccendo tutti, 
+        a cosa pensano, con chi sono.  `,
+        position: 'center bottom'
+    },
+    {
+        title: 'Alan Watts',
+        header: '\"Vuoto e meraviglioso\"',
+        author: 'Dorin David',
+        author_path: '/autori/dorin_david',
+        article_path: '/articoli/zen-alan-watts',
+        img: '/assets/Alan-Watts.jpg',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a velit gravida, tristique leo quis, lacinia quam. Praesent a metus quis turpis blandit tristique. Cras vitae urna vulputate, porttitor elit et, luctus mi.',
+        //set logic for tags
+        tags: ['dorin david', 'filosofia', 'zen', 'alan watts',]
+    },
+    {
+        title: 'Witold Gombrowicz',
+        header: 'Enfant terrible',
+        author: 'Dorin David',
+        author_path: 'autori/dorin_david',
+        article_path: '/articoli/gombrowicz',
+        img: '/assets/gombrowicz.jpg',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a velit gravida, tristique leo quis, lacinia quam. Praesent a metus quis turpis blandit tristique. Cras vitae urna vulputate, porttitor elit et, luctus mi.',
+        position: 'center bottom'
+    },
 
 
 ]
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,53 @@
+import { useState, useEffect } from 'react';
+import ReactPaginate from 'react-paginate';
 import { database } from '../mock-database';
 import ArticlesList from '../components/Articles/ArticlesList';
+import style from '../styles/components/pagination.module.css'
 
 
 function Home() {
+  const [perPage] = useState(5);
+  const [pageCount, setPageCount] = useState(0);
+  const [offset, setOffset] = useState(0);
+  const [articles, setArticles] = useState([])
+
+  
+  const handlePageClick = (e) => {
+    const selectedPage = e.selected;
+    setOffset(selectedPage)
+    window.scrollTo(0, 0)
+};
+
+
+  useEffect(() => {
+    setPageCount(Math.floor(database.length / perPage))
+    let data = null;
+    if(offset > 0){
+       data = database.slice((offset * 5) + 1, (offset * 5) + (perPage + 1))
+    } else {
+      data = database.slice(0, 5)
+    }
+    setArticles(data)
+
+  }, [offset])
+
+
   return (
     <div >
-      <ArticlesList articles={database} />
+      <ArticlesList articles={articles} />
+      <ReactPaginate
+        previousLabel={"prev"}
+        nextLabel={"next"}
+        breakLabel={"..."}
+        breakClassName={"break-me"}
+        pageCount={pageCount}
+        marginPagesDisplayed={2}
+        pageRangeDisplayed={5}
+        onPageChange={handlePageClick}
+        containerClassName={style.pagination}
+        subContainerClassName={"pages pagination"}
+        activeClassName={style.active} 
+        />
     </div>
   )
 }
