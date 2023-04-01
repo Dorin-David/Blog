c49916140877eecc033486ea6acd17a0fe06e713
Dorin David <doriodavid@gmail.com>
2023-04-01 10:30:36 +0100

    [strapi] add environmental vars for API; create fecther component for Articles

---
+++ components/Articles/Articles.js
@@ -0,0 +1,30 @@
+import { useEffect, useState  } from 'react';
+
+function Articles(props){
+    const API = process.env.NEXT_PUBLIC_CMS_API;
+    const [articles, setArticles] = useState(null);
+    const [error, setError] = useState( false );
+    const [loading, setLoading] = useState(null);
+
+
+    useEffect(() => {
+        setError(false);
+        setLoading(true);
+        async function fetchArticles(){
+
+            try {
+                const req = await fetch( `${API}/articles` );
+                const data = await req.json();
+
+                console.log(data);
+            } catch(error){
+                setError( 'An error occured' )
+            }
+
+            setLoading(false)
+        }
+
+        fetchArticles()
+    }, [])
+}
+export default Articles
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
 import { database } from '../mock-database';
 import StaticArticlesList from '../components/Articles/StaticArticlesList';
+import Articles from '../components/Articles/Articles';
 
-const Home = () => <StaticArticlesList articles={database} />;
+const API = process.env.NEXT_PUBLIC_CMS_API;
+
+const Home = () => <Articles />;
 
 export default Home
