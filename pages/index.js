6c9e0b1185fcfb65a446e215ec6b88f76b6274bc
Dorin David <doriodavid@gmail.com>
2023-04-01 10:38:52 +0100

    [strapi] move legacy articles to LegacyArticles folder (will be deleted)

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,3 @@
-import { database } from '../mock-database';
-import StaticArticlesList from '../components/Articles/StaticArticlesList';
 import Articles from '../components/Articles/Articles';
 
 const API = process.env.NEXT_PUBLIC_CMS_API;
