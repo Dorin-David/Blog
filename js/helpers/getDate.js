dac0a9a5a70c1e91da30d1c6bb123592cec5fd2c
Dorin David <doriodavid@gmail.com>
2023-05-06 18:02:19 +0100

    create helpers folder; add date parser

---
+++ js/helpers/getDate.js
@@ -0,0 +1,10 @@
+/**
+ * Parses a ISO 8601 formatted date into a human readable. day/month/year format date
+ * E.g. parseDate('2023-03-25T17:13:58.058Z') -> "25/03/2023"
+ * @param {String} timestamp - the iso date timestampt to be parsed
+*/
+export function parseDate(timestamp) {
+    const dateObj = new Date(timestamp);
+
+    return dateObj.toLocaleDateString('en-GB');
+}
