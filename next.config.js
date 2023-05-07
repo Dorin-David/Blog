fcdd1835ce4d90c38409120bcae81c1d7837d2b2
Dorin David <doriodavid@gmail.com>
2023-05-07 22:07:55 +0100

    create global variables folder; tweak project config to prepend them to sass modules

--- next.config.js
+++ next.config.js
@@ -3,5 +3,6 @@ const path = require('path');
 module.exports = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
+    prependData: `@import "styles/global/variables.scss"; @import "styles/global/colors.scss";`
   },
 };
