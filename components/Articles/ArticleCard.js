54ec8f3d15967f3b51ab7aba8a8a8ff3e4239352
Dorin David <doriodavid@gmail.com>
2023-05-07 11:41:41 +0100

    add author to articles card

--- components/Articles/ArticleCard.js
+++ components/Articles/ArticleCard.js
@@ -2,11 +2,13 @@ import { parseDate } from "../../js/helpers/getDate";
 
 function ArticleCard( props ){
     const { article } = props;
+    const author = article.createdBy.firstname + article.createdBy.lastname; 
     const published = parseDate( article.publishedAt );
     return (
     <article>
         <h3>{article.Title}</h3>
         {article.Subtitle && <h4>{article.Subtitle}</h4>}
+        <p>{author}</p>
         <time dateTime={published}>{published}</time>
     </article>)
 }
